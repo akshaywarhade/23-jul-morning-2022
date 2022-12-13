16a5e10ea00af467cc4907a690d4be0909ccfc3a
unknown <akshaywarhade11@gmail.com>
2022-12-13 10:22:27 +0530

    changes

--- src/test/java/NopCommerceBase/Base.java
+++ src/test/java/NopCommerceBase/Base.java
@@ -13,8 +13,6 @@ public class Base
 	  {
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\AITS\\\\eclipse-workspace\\\\23Jul B Morning\\\\selenium-java-4.4.0\\\\chromedriver.exe");
 	    driver = new ChromeDriver();
-	    
-	    
 		driver.get(utility.readdatafrompropertyfile("url"));
 		driver.manage().window().maximize();
       }
