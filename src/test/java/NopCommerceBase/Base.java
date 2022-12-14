0b16182d8d446dfccf9afb3b8b4f7cf0915b191a
unknown <akshaywarhade11@gmail.com>
2022-12-14 10:26:42 +0530

    Add Comment

--- src/test/java/NopCommerceBase/Base.java
+++ src/test/java/NopCommerceBase/Base.java
@@ -11,7 +11,7 @@ public class Base
 	protected static  WebDriver driver;
 	public void LaunchNopCommerce() throws IOException
 	  {
-		
+		//launching browser
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\AITS\\\\eclipse-workspace\\\\23Jul B Morning\\\\selenium-java-4.4.0\\\\chromedriver.exe");
 	    driver = new ChromeDriver();
 		driver.get(utility.readdatafrompropertyfile("url"));
